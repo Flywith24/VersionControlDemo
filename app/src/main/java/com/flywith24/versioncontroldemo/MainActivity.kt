280555243df2a7eaee129398ece356d456225dd8
fly_with24 <fly_with24@163.com>
2020-05-27 11:08:21 +0800

    Initial commit

---
+++ app/src/main/java/com/flywith24/versioncontroldemo/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.flywith24.versioncontroldemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+}
---
+++ version/src/main/java/com/flywith24/version/DependencyVersionPlugin.kt
@@ -0,0 +1,16 @@
+package com.flywith24.version
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @author yyz (杨云召)
+ * @date   2020/5/27
+ * time   8:50
+ * description
+ */
+class DependencyVersionPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+
+    }
+}
